a71878184dda2843badea3b818c8563a1abd89bb
ErikTeixeira <erik.barbosa515@gmail.com>
2024-04-18 21:20:28 -0300

    criacao responses

---
+++ src/main/java/br/com/fiap/simuladospringpjunidades/dto/response/UnidadeResponse.java
@@ -0,0 +1,14 @@
+package br.com.fiap.simuladospringpjunidades.dto.response;
+
+import lombok.Builder;
+
+@Builder
+public record UnidadeResponse(
+        Long id,
+        String nome,
+        String sigla,
+        String descricao,
+        UnidadeResponse macro
+
+) {
+}
